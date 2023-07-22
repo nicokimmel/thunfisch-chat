7c1fce91ea63018eab3b2bcd54e66f03cb523293
Nico Kimmel <mail@nicokimmel.de>
2023-07-22 11:44:16 +0200

    Implemented basic google search

--- src/public/js/api.js
+++ src/public/js/api.js
@@ -1,9 +1,9 @@
-function callAPI(model, messages, callback) {
+function prompt(model, messages, callback) {
     let api = new XMLHttpRequest()
     api.open("POST", "/api", true);
     api.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
     api.send(JSON.stringify({ "model": model, "messages": messages }))
-    
+
     api.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
@@ -12,3 +12,16 @@ function callAPI(model, messages, callback) {
     }
 }
 
+function search(query, callback) {
+    let api = new XMLHttpRequest()
+    api.open("POST", "/search", true);
+    api.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    api.send(JSON.stringify({ "query": query }))
+
+    api.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+            callback(this.responseText)
+        }
+    }
+}
